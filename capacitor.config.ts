3b1a6dde1320c7771046b88903339ed7068f64e2
akshatjain <f20180740@pilani.bits-pilani.ac.in>
2025-06-28 03:25:20 +0530

    changed capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.arihantmedigens.app',
   appName: 'Arihant Medigens',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+      SplashScreen: {
+          launchShowDuration: 2000,
+          backgroundColor: "#ffffff",
+          showSpinner: false,
+          androidSpinnerStyle: "small",
+          splashFullScreen: true,
+          splashImmersive: true,
+          },
+      },
 };
 
 export default config;
